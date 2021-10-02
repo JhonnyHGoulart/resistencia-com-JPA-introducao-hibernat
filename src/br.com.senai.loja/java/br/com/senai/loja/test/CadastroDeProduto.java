d2e9af49fe633912c9a78d6caeaff131d17b9f38
Jhonny Hilário Goulart <jhonny.goulart@sc.senai.br>
2021-10-02 13:25:19 -0300

    Renomeando o arquivo persistence.xml

--- src/br.com.senai.loja/java/br/com/senai/loja/test/CadastroDeProduto.java
+++ src/br.com.senai.loja/java/br/com/senai/loja/test/CadastroDeProduto.java
@@ -1,10 +1,10 @@
 package br.com.senai.loja.test;
 
+import br.com.senai.loja.DAO.ProdutoDAO;
 import br.com.senai.loja.modelo.Produto;
+import br.com.senai.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 public class CadastroDeProduto {
     public static void main(String[] args) {
@@ -13,13 +13,13 @@ public class CadastroDeProduto {
         celular.setDescricao("6 GB RAM");
         celular.setPreco(8000.0);
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
-        EntityManager em = factory.createEntityManager();
+        EntityManager em = JPAUtil.getEntityManager();
+
+        ProdutoDAO dao = new ProdutoDAO(em);
 
         em.getTransaction().begin();
-        em.persist(celular);
+        dao.cadastrar(celular);
         em.getTransaction().commit();
         em.close();
-        factory.close();
     }
 }
